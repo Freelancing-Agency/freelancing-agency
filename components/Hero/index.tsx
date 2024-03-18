3b214a35544d69c1146feadead08065cc2e7aeed
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2024-03-18 16:33:37 +0530

    update

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,13 +16,13 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+             🔥 Welcome to Our Digital Playground
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
+                Software Agency - Your Gateway to Seamless Digital Experiences. {"   "}
+                {/* <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
                   SaaS
-                </span>
+                </span> */}
               </h1>
               <p>
                 Solid Pro - Packed with all the key integrations you need for
--- types/blog.ts
+++ types/blog.ts
@@ -17,3 +17,4 @@ export type Blog = {
   tags?: string[];
   publishedAt?: string;
 };
+
