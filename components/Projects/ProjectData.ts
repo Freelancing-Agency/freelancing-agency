5e1877b8de5f27b53538974abd9da308f930e2d6
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2024-04-10 17:53:07 +0530

    new commit

--- components/Projects/ProjectData.ts
+++ components/Projects/ProjectData.ts
@@ -1,5 +1,10 @@
 import p1 from "../../public/images/projects/p1.png"
 import p2 from "../../public/images/projects/p2.png"
+// import p3 from "../../public/images/projects/pro_codestrick.png"
+// import p4 from "../../public/images/projects/pro_prompt.png"
+// import p5 from "../../public/images/projects/pro_rest.png"
+// import p6 from "../../public/images/projects/rk.png"
+
 import { Testimonial } from "@/types/testimonial";
 
 export const ProjectsData: Testimonial[] = [
@@ -39,8 +44,9 @@ export const ProjectsData: Testimonial[] = [
     id: 5,
     name: "Jhon Abraham",
     designation: "Founter @democompany",
-    image: p2,
+    image: p1,
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
   },
 ];
+
