6b5eb95a5147f5d85da49b0c1e6109d825001542
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2024-04-13 00:24:22 +0530

    update

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -39,14 +39,14 @@ const Header = () => {
         <div className="flex w-full items-center justify-between xl:w-1/4">
           <a href="/">
             <Image
-              src="/images/logo/logo-dark.svg"
+              src="/images/logo/logo_w.png"
               alt="logo"
               width={150}
               height={50}
               className="hidden dark:block w-50"
             />
             <Image
-              src="/images/logo/logo-light.svg"
+              src="/images/logo/logo_b.png"
               alt="logo"
               width={150}
               height={50}
