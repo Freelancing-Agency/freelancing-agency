4a6bbd166cdd5c37ff9e761cd99e5f9bafb8e628
saigenix <sainathmahindrakar@gmail.com>
2024-04-13 00:27:22 +0530

    adjusted hero section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,10 +15,10 @@ const Hero = () => {
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
-              <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
+              <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white ">
               Empower Your Business with Expert Software Solutions
               </h4>
-              <h3 className="mb-5 pr-16 text-1xl font-bold text-black dark:text-white xl:text-hero ">
+              <h3 className="mb-5 pr-16 text-4xl font-bold text-black dark:text-white xl:text-hero">
                 Digitalize ⚡ your Business with Advance Solutions.{"   "}
                 {/* <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
                 </span> */}
